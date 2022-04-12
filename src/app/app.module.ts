68a974b44c3d5bb10e5fcba628230fb71721c373
Kuba Olszewski <kubar7kubar@gmail.com>
2022-04-12 13:35:27 +0200

    add navbar with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, MainComponent],
-  imports: [BrowserModule, ReactiveFormsModule, NgbModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    NgbModule,
+    RouterModule.forRoot([
+      { path: '', component: MainComponent },
+      { path: 'summary', component: MainComponent },
+      { path: 'freedays', component: MainComponent },
+      { path: 'declarations', component: MainComponent },
+      { path: 'settings', component: MainComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
